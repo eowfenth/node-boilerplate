d854dd00069d296c3ef3ae8da1cc6c092de93eae
Nicolas 'eowfenth' Deçordi dos Reis <eowfenth.services@gmail.com>
2020-03-02 21:35:46 -0300

    feat: add initial http server setup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { RequestListener } from 'http';
+
+/**
+ * Handles the requests and call for the router;
+ * @param request - http.ServerRequest entity
+ * @param response - http.ServerResponse entity
+ */
+const app_controller: RequestListener = (request, response) => {
+};
+
+export default app_controller;
